0bb93931882af2a390bcde2c6fd86f86330453f6
Elisa ROMAIN <romain.elisa@yahoo.fr>
2020-04-06 10:41:03 +0200

    first commit

---
+++ MEF_Quest/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace MEF_Quest
+{
+    class Program
+    {
+        private Mef _host;
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run();
+
+            Console.ReadKey();
+        }
+
+        private void Run()
+        {
+            _host = new Mef();
+            HelloSayer hello = _host.Container.GetExportedValue<HelloSayer>();
+        }
+            
+
+    }
+
+    internal class Mef
+    {
+        public CompositionContainer Container
+        {
+            get
+            {
+                if (_container == null)
+                {
+                    var catalog =
+                        new DirectoryCatalog(".", "MEF_Quest.*");
+
+                    _container = new CompositionContainer(catalog);
+                }
+
+                return _container;
+            }
+        }
+        private CompositionContainer _container = null;
+    }
+
+    [Export]
+    internal class HelloSayer
+    {
+        public HelloSayer()
+        {
+            SayHello();
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Bonjour !");
+        }
+    }
+
+
+}
